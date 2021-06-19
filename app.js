03aef8e1094c116bcbac209c8a03bd8eda8727dc
xxria17 <xxrcy32@gmail.com>
2021-06-19 21:23:36 +0900

    모두, git, 도움링크 필터 구현

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var files = ["data/class.json", "data/quiz.json"];
-
+var filter = 'all';
+var quizFilter = 'quizall';
+const tbody = document.querySelector(".tbody");
+const qbody = document.querySelector(".quiz_body")
+const tTr = document.querySelectorAll(".tbody tr")
 // json 파일 읽기
 function readJsonFile() {
     for (let i = 0; i < files.length; i++) {
@@ -11,8 +15,6 @@ function readJsonFile() {
 }
 
 function displayClassItems(data) {
-    const tbody = document.querySelector(".tbody");
-    const qbody = document.querySelector(".quiz_body")
 
     if (data.length > 13) {
         tbody.innerHTML = data.map((item, index)=> createHTML(item, index)).join("");
@@ -23,16 +25,133 @@ function displayClassItems(data) {
 }
 
 function createHTML(item, index) {
-    return `
+    let html = ''
+
+    if (filter == 'all') {
+        html = `
     <tr>
     <th scope="row">${index+1}</th>
-    <td>${item.title}</td>
-    <td><a href="${item.docUrl}" class="badge bg-secondary">문서</a>
-    </td>
-    <td>${createLinkHTML(item.links)}</td>
-    <td>${item.date}</td>
-    <td><a href="${item.gitUrl}" class="badge bg-secondary">git</a>
-    </tr>`
+    <td>${item.title}</td>`
+
+        if (item.docUrl.length != 0) {
+            html += `
+            <td><a href="${item.docUrl}" class="badge bg-secondary">문서</a>
+            </td>
+            <td>${createLinkHTML(item.links)}</td>
+            <td>${item.date}</td>`
+
+            
+        if (item.gitUrl.length != 0) {
+            html += `<td><a href="${item.gitUrl}" class="badge bg-secondary">git</a>
+            </td>
+            </tr>`
+        } else {
+            html += `<td></td>
+            </tr>`
+        }
+        } else {
+            html += `<td></td>
+            <td>${createLinkHTML(item.links)}</td>
+            <td>${item.date}</td>`
+            
+        if (item.gitUrl.length != 0) {
+            html += `<td><a href="${item.gitUrl}" class="badge bg-secondary">git</a>
+            </td>
+            </tr>`
+        } else {
+            html += `<td></td>
+            </tr>`
+        }
+        }
+        
+        
+    } else if (filter == 'link') {
+
+        if (item.links.length != 0) {
+            html = `
+        <tr>
+        <th scope="row">${index+1}</th>
+        <td>${item.title}</td>`
+            if (item.docUrl.length != 0) {
+                html += `
+                <td><a href="${item.docUrl}" class="badge bg-secondary">문서</a>
+                </td>
+                <td>${createLinkHTML(item.links)}</td>
+                <td>${item.date}</td>`
+    
+                
+            if (item.gitUrl.length != 0) {
+                html += `<td><a href="${item.gitUrl}" class="badge bg-secondary">git</a>
+                </td>
+                </tr>`
+            } else {
+                html += `<td></td>
+                </tr>`
+            }
+            } else {
+                html += `<td></td>
+                <td>${createLinkHTML(item.links)}</td>
+                <td>${item.date}</td>`
+                
+            if (item.gitUrl.length != 0) {
+                html += `<td><a href="${item.gitUrl}" class="badge bg-secondary">git</a>
+                </td>
+                </tr>`
+            } else {
+                html += `<td></td>
+                </tr>`
+            }
+            }
+        } 
+    } else if (filter == 'recent') {
+        html = `
+        <tr>
+        <th scope="row">${index+1}</th>
+        <td>${item.title}</td>`
+        if (item.docUrl.length != 0) {
+            html += `
+            <td><a href="${item.docUrl}" class="badge bg-secondary">문서</a>
+            </td>
+            <td>${createLinkHTML(item.links)}</td>
+            <td>${item.date}</td>`
+            
+        if (item.gitUrl.length != 0) {
+            html += `<td><a href="${item.gitUrl}" class="badge bg-secondary">git</a>
+            </td>
+            </tr>`
+        } else {
+            html += `<td></td>
+            </tr>`
+        }
+        } else {
+            html += `<td></td>
+            <td>${createLinkHTML(item.links)}</td>
+            <td>${item.date}</td>`
+            
+        if (item.gitUrl.length != 0) {
+            html += `<td><a href="${item.gitUrl}" class="badge bg-secondary">git</a>
+            </td>
+            </tr>`
+        } else {
+            html += `<td></td>
+            </tr>`
+        }
+        }
+    } else {
+        if (item.gitUrl.length != 0) {
+            html = `
+            <tr>
+            <th scope="row">${index+1}</th>
+            <td>${item.title}</td>
+            <td><a href="${item.docUrl}" class="badge bg-secondary">문서</a>
+            </td>
+            <td>${createLinkHTML(item.links)}</td>
+            <td>${item.date}</td>
+            <td><a href="${item.gitUrl}" class="badge bg-secondary">git</a>
+            </tr>`
+        }
+    }
+   return html
 }
 
 function createLinkHTML(data) {
@@ -44,14 +163,69 @@ function createLinkHTML(data) {
 }
 
 function createQuizHTML(item) {
-    return `
-    <tr>
-    <td>${item.title}</td>
-    <td><a href="${item.docUrl}" class="badge bg-secondary">문서</a>
-    </td>
-    <td><a href="${item.previewUrl}" class="badge bg-secondary">previewUrl</a>
-    <td><a href="${item.gitUrl}" class="badge bg-secondary">git</a>
-    </tr>`
+    let html = '';
+    if (quizFilter == 'quizall') {
+        html = `
+        <tr>
+        <td>${item.title}</td>
+        <td><a href="${item.docUrl}" class="badge bg-secondary">문서</a>
+        </td>
+        <td><a href="${item.previewUrl}" class="badge bg-secondary">previewUrl</a>
+        <td><a href="${item.gitUrl}" class="badge bg-secondary">git</a>
+        </tr>`;
+    } else {
+        if (item.gitUrl.length != 0) {
+            html = `
+            <tr>
+            <td>${item.title}</td>
+            <td><a href="${item.docUrl}" class="badge bg-secondary">문서</a>
+            </td>
+            <td><a href="${item.previewUrl}" class="badge bg-secondary">previewUrl</a>
+            <td><a href="${item.gitUrl}" class="badge bg-secondary">git</a>
+            </tr>`;
+        } 
+    }
+        
+    return html;
 }
 
 readJsonFile();
+
+function selectTab(event) {
+    var i, tabBtn;
+    tabBtn = document.getElementsByClassName("btn");
+    for (i = 0; i < tabBtn.length; i++) {
+        tabBtn[i].className = tabBtn[i].className.replace(" active", "");
+    }
+
+    if (event.currentTarget.value == 'all' || event.currentTarget.value == 'link' || event.currentTarget.value == 'git' || event.currentTarget.value == 'recent') {
+        filter = event.currentTarget.value
+        
+        if (quizFilter == 'quizall') {
+            tabBtn[4].className += " active";
+        } else {
+            tabBtn[5].className += " active";
+        }
+    } else {
+        quizFilter = event.currentTarget.value;
+        if (filter == 'all') {
+            tabBtn[0].className += " active";
+        } else if (filter == 'link') {
+            tabBtn[1].className += " active";
+        } else if (filter == 'git') {
+            tabBtn[2].className += " active";
+        } else {
+            tabBtn[3].className += " active";
+        }
+    }
+    event.currentTarget.className += " active";
+    readJsonFile();
+}
+
+// 최신순 정렬
+function sort() {
+    Array.from(tTr).reverse().forEach((el, i) => {
+        tbody.append(el);
+    })
+}
+
